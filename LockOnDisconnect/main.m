9d7ef4bbe3b54ab360f6417f5a4dde03d4b501f8
Eddie Kelley <eddie@kelleycomputing.net>
2013-10-19 13:37:35 -0700

    Added GNU GPLv3 license

--- LockOnDisconnect/main.m
+++ LockOnDisconnect/main.m
@@ -5,6 +5,20 @@
 //  Created by Eddie Kelley on 10/15/13.
 //  Copyright (c) 2013 Kelley Computing. All rights reserved.
 //
+//	This file is part of LockOnDisconnect.
+//
+//    LockOnDisconnect is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Foobar is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with LockOnDisconnect.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Foundation/Foundation.h>
 #import <AppKit/NSWorkspace.h>
